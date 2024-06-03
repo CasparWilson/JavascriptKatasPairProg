763919b9ac425b5b7ff70c4ff31b0a7776311459
2405-006-CW <2405-006-cw@U-2B5MB7QJAWMTD.corp.amazonworkspaces.com>
2024-06-03 14:28:24 +0100

    JSDoc added for getMostCommonLetters.js

--- src/getMostCommonLetters.js
+++ src/getMostCommonLetters.js
@@ -1,3 +1,12 @@
+
+/**
+ * takes a string and returns an array of 2 element arrays, where the first member is a letter in the string, and the second is the number 
+ * of times it appears in the string. The return array consists of arrays with the highest second member, up to a maximum of numberOfWinners
+ * 
+ * @param {string} inputText 
+ * @param {number} numberOfWinners 
+ * @returns {object} array of number of Winners many 2-element arrays, consisting of must occurant letters, and the number of times it occurs
+ */
 function getMostCommonLetters(inputText, numberOfWinners){
 const countObject = {}
 for(let letter of inputText){
